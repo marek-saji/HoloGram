4b6dd2027dc70794a8a5a1e8384a16f3772bd093
Marek Augustynowicz <m.augustynowicz@holonglobe.com>
2010-03-18 17:17:02 +0100

    updating hg.definitions as in BDB it actually is used

--- htdocs/js/hg.definitions.js
+++ htdocs/js/hg.definitions.js
@@ -1,32 +1,16 @@
-// "ajax core"
-hg['ajax'] = {js:'../hg/js/hg.ajax.js'};
-hg['ajaxHandleResponse'] = {js:'../hg/js/hg.ajax.js'};
-hg['ajaxDOMReady'] = {js:'../hg/js/hg.ajax.js'};
-
-// dialog boxes
-hg['nyroModalInit'] = {js:'../hg/js/hg.nyroModal.js'};
-hg['prompt']  = {js:'hg.prompt.js'};
-hg['confirm'] = {js:'hg.prompt.js'};
-
-// help tooltips (should be called only from live events)
-hg['tooltip']     = {js:'hg.tooltip.js'};
-hg['tooltipHide'] = {js:'hg.tooltip.js'};
-
-// modal dialog boxes (should be called from live events)
-hg['modalDialog']       = {js:'hg.modalDialog.js'};
-hg['modalDialogImg']    = {js:'hg.modalDialog.js'};
-
-// flash visibility toggler
-hg['hideFlashes']   = {js:'hg.flashtoggler.js'};
-hg['unhideFlashes'] = {js:'hg.flashtoggler.js'};
-
 // googlemaps
-hg['gmapsPlaceMarker']  = {js:'hg.gmaps.js'};
-hg['gmapsGoto']         = {js:'hg.gmaps.js'};
-hg['gmapsGetLocation']  = {js:'hg.gmaps.js'};
-hg['gmapsGetSpan']      = {js:'hg.gmaps.js'};
-hg['gmapsDelayedEventPlaceMarkers'] = {js:'hg.gmaps.js'};
-hg['gmapsPlaceMarkers'] = {js:'hg.gmaps.js'};
+hg['gmapsSetup']        = {js:'../hg/js/hg.gmaps.js'};
+hg['gmapsDelayedUpdate']= {js:'../hg/js/hg.gmaps.js'};
+hg['gmapsUpdate']       = {js:'../hg/js/hg.gmaps.js'};
+hg['gmapsPlaceMarker']  = {js:'../hg/js/hg.gmaps.js'};
+hg['gmapsGoto']         = {js:'../hg/js/hg.gmaps.js'};
+hg['gmapsGetLocation']  = {js:'../hg/js/hg.gmaps.js'};
+hg['gmapsGetSpan']      = {js:'../hg/js/hg.gmaps.js'};
+hg['gmapsDelayedEventPlaceMarkers'] = {js:'../hg/js/hg.gmaps.js'};
+hg['gmapsPlaceMarkers']     = {js:'../hg/js/hg.gmaps.js'};
+hg['gmapsGetDirections']    = {js:'../hg/js/hg.gmaps.js'};
+hg['gmapsGetMarkersCenter'] = {js:'../hg/js/hg.gmaps.js'};
+
 
 // editor (xinha)
 hg['editor_init'] = {js:'hg.editor.js'};
@@ -34,4 +18,19 @@ hg['editor_init'] = {js:'hg.editor.js'};
 //forms
 hg['input_validate'] = {js:'../hg/js/hg.formsvalidate.js'};
 hg['form_validate'] = {js:'../hg/js/hg.formsvalidate.js'};
+
+// default help value (should be called once, will bind via live events)
+hg['helpval'] = {js:'../hg/js/hg.helpval.js'};
+
+// "ajax core"
+hg['ajax'] = {js:'../hg/js/hg.ajax.js'};
+hg['ajaxHandleResponse'] = {js:'../hg/js/hg.ajax.js'};
+hg['ajaxDOMReady'] = {js:'../hg/js/hg.ajax.js'};
+
+// dialog boxes
+hg['nyroModalInit'] = {js:'../hg/js/hg.nyroModal.js'};
+
+// flash visibility toggler
+hg['hideFlashes']   = {js:'hg.flashtoggler.js'};
+hg['unhideFlashes'] = {js:'hg.flashtoggler.js'};
 
