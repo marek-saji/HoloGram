7b878033ae9c27ad6bd2ac5cd6db0e766f66df67
Marek Augustynowicz <m.augustynowicz@holonglobe.com>
2010-04-06 20:09:03 +0200

    special template for deleting images
    dispalys "delete" checkbox plus image preview.
    shows only if image supplied

---
+++ tpl/Forms/FImageFile_del.php
@@ -0,0 +1,27 @@
+<?php
+/**
+ * Displays "delete image" option
+ *
+ * when image passed, will render it above the checkbox
+ * @author m.augustynowicz
+ *
+ * @param array $image {@see tpl/uploaded_image} for details
+ * @param string $image_size {@see tpl/uploaded_image} for details
+ */
+extract(array_merge(
+    array(
+        'image' => null,
+        'image_size' => 'original',
+    ),
+    $____local_variables
+), EXTR_REFS|EXTR_PREFIX_INVALID, 'param');
+
+if (!@$image['id'])
+    return false;
+
+$this->inc('uploaded_image', array(
+    $image,
+    'size' => $image_size
+));
+return $t->inc('Forms/FBool', $____local_variables);
+
