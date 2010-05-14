5ea1468057f73c646e8872630fe8db9b16f81329
Michal Jutkiewicz <m.jutkiewicz@holonglobe.com>
2010-05-14 14:22:51 +0200

    some unnecessary things deleted

--- lib/MediaUploadModel.php
+++ lib/MediaUploadModel.php
@@ -239,9 +239,8 @@ class MediaUploadModel extends UploadModel
     {
         $f = g('Functions');
 
-        $path_mp3 = escapeshellarg($path . '.mp4');
-        $path_mp4 = escapeshellarg($path . '.jpeg');
-        $path_jpeg = escapeshellarg($path . '.frame.jpeg');
+        $path_mp4 = escapeshellarg($path . '.mp4');
+        $path_jpeg = escapeshellarg($path . '.frame.jpg');
         $path_thumb = escapeshellarg($path . '.%dx%d.png');
         $path = escapeshellarg($path);
 
@@ -260,7 +259,7 @@ class MediaUploadModel extends UploadModel
                     . ' -ab '.$ab
                     . ' -b '.$vb
                     . ' -s '.$vs
-                    . ' '.$path_mp3,
+                    . ' '.$path_mp4,
                 $output, $return_value
             );
         if (0 != $return_value)
