37f7561e2591bf7cd2479162f87025377ccc3053
Bartlomiej Matuszewski <b.matuszewski@holonglobe.com>
2010-07-02 15:11:15 +0200

    $('.date') fields validated on change instead of blur @author m.augustynowicz

--- tpl/Forms/FDate.php
+++ tpl/Forms/FDate.php
@@ -5,6 +5,7 @@
  * For parameters refer to FString
  */
 @$____local_variables['class'] .= ' date';
+@$____local_variables['validate_js_event'] = 'change';
 
 return $this->inc('Forms/FString', $____local_variables);
 
