62eb9f257219aa4dd17f8231fed86e7e3e02c480
Marek Augustynowicz <m.augustynowicz@holonglobe.com>
2010-09-05 21:40:24 +0200

    Paginator controller, copied from POP app

---
+++ lib/PaginatorController.php
@@ -0,0 +1,136 @@
+<?php
+
+/**
+ * Paginating for DataSet
+ *
+ * USAGE:
+ * add as a child and then (e.g. named 'p'):
+ * <code>
+ * // set margins on a model
+ * $this->getChild('p')->setMargin($model);
+ * // or, to change default results per page setting:
+ * $this->getChild('p')->setMargin($model, 42);
+ * // or just get pagination data (won't touch $model)
+ * $this->getChild('p')->config($model);
+ * $margins = $this->getChild('p')->getMargins();
+ * // or get pagination settings for 1000 objects (42 per page)
+ * $this->getChild('p')->config(1000, 42)->getMargins();
+ * </code>
+ *
+ * @author m.augustynowicz
+ */
+class PaginatorController extends Component
+{
+    /**
+     * Number of items
+     */
+    protected $_count = null;
+    /**
+     * Number of items per page
+     */
+    protected $_perpage = 7;
+    /**
+     * Page we are on.
+     */
+    protected $_current_page = 1;
+    /**
+     * Total pages
+     */
+    protected $_total_pages = null;
+
+    /**
+     * Count things.
+     * 
+     * @param null|DataSet|integer $count_source when DataSet, will call it's
+     *        getCount() to obtain item count
+     * @param null|integer $perpage number of items per page
+     *
+     * @return PaginatorController $this
+     */
+    public function config($count_source = null, $perpage = null)
+    {
+        $this->_setTemplate($this->_default_action);
+        if(null !== $count_source)
+        {
+            if(is_a($count_source, 'DataSet'))
+                $this->_count = $count_source->getCount();
+            else
+                $this->_count = $count_source;
+        }
+        if(null !== $perpage)
+            $this->_perpage = $perpage;
+        /** @todo maybe implement obtaining $perpage from parent */
+        /*
+        else if (method_exists($this->getParent(), 'getPerpage'))
+            $this->_perpage = $this->getParent()->getPerpage();
+         */
+        $this->_total_pages = ceil($this->_count / $this->_perpage);
+        return $this;
+    }
+
+    /**
+     * Set margins on given DataSet
+     *
+     * @param DataSet $ds
+     * @param null|integer $perpage passed to {@see config()}
+     *
+     * @return DataSet $ds, to allow chaining
+     */
+    public function setMargins(DataSet $ds, $perpage = null)
+    {
+        if(null === $this->_count)
+            $this->config($ds, $perpage);
+        $margins = $this->getMargins();
+        return $ds->setMargins($margins[0], $margins[1]);
+    }
+
+    /**
+     * Get margin boundaries
+     *
+     * @return array
+     */
+    public function getMargins()
+    {
+        $from = ($this->_current_page - 1) * $this->_perpage;
+        $to = $from + $this->_perpage;
+        return array(
+            $from,
+            $to
+        );
+    }
+
+    /**
+     * Nothing much to explain
+     *
+     * @param array $params URL params
+     */
+    public function defaultAction(array $params)
+    {
+        if(!$this->_current_page = (int)@$params[0])
+            $this->_current_page = 1;
+    }
+
+    public function getCurrentPage()
+    {
+        return $this->_current_page;
+    }
+
+    /**
+     * Render enhanced with caching.
+     * @return void
+     */
+    public function render()
+    {
+        static $cache = null;
+        if(null != $cache)
+        {
+            echo $cache[1];
+            return $cache[0];
+        }
+        ob_start();
+        $cache = array(
+            parent::render(),
+            ob_get_flush()
+        );
+    }
+}
---
+++ tpl/Paginator/default.php
@@ -0,0 +1,133 @@
+<?php
+/**
+ * Pagination
+ * @author m.augustynowicz
+ *
+ * @uses $this->_count
+ * @uses $this->_perpage
+ * @uses $this->_total_pages
+ * @uses $this->_current_page
+ */
+
+$v->addCss($t->file('paginator', 'css'));
+
+// we will show $neighbour pages on the left and on the right of current page
+$neighbour = 5;
+
+if (!$t->_count || $t->_total_pages <= 1)
+{
+    echo '<div class="empty paginator"></div>';
+    return 0;
+}
+?>
+
+<div class="paginator">
+<?php
+
+$total_pages = $t->_count/$t->_perpage;
+
+$start = max($this->_current_page - $neighbour, 0);
+$stop  = min($this->_current_page + $neighbour, $this->_total_pages+1);
+if (($tmp = $this->_current_page - $start) < $neighbour)
+{
+    $stop = min($stop+($neighbour-$tmp), $this->_total_pages+1);
+}
+if (($tmp = $stop - $this->_current_page) < $neighbour)
+{
+    $start = max($start-($neighbour-$tmp), 0);
+}
+
+// generate pages list
+
+$pages = array($t->_current_page => $t->_current_page);
+#for (list($a,$b)=array(0,1) ; $neighbour > $c = $a+$b ; $a=$b, $b=$c) // fibbonachi, yay!
+for ($c = 0 ; $neighbour*3 > $c++ ;)
+{
+    if ($stop  > $x = $t->_current_page + $c)
+        $pages[$x] = $x;
+    if ($start < $x = $t->_current_page - $c)
+        $pages[$x] = $x;
+}
+asort($pages);
+
+
+// first/prev/next/last
+
+$label_first = 'first page';
+$label_prev = '&lt;';
+$label_next = '&gt;';
+$label_last = 'last page';
+
+$ppages = array();
+$npages = array();
+//$ppages[$label_first] = 1;
+$ppages[$label_prev] = $t->_current_page-1;
+if (reset($pages) != 1)
+{
+    unset($pages[key($pages)]);
+    reset($pages);
+    $ppages[1] = 1;
+    if (current($pages) != 1+1)
+    {
+        unset($pages[key($pages)]);
+        $ppages['&hellip'] = null;
+    }
+}
+if (end($pages) != $t->_total_pages)
+{
+    if (current($pages) != $t->_total_pages - 1)
+    {
+        unset($pages[key($pages)]);
+        end($pages);
+        $npages['&hellip;'] = null;
+    }
+    unset($pages[key($pages)]);
+    $npages[$t->_total_pages] = $t->_total_pages;
+}
+$npages[$label_next] = $t->_current_page+1;
+//$npages[$label_last] = $t->_total_pages;
+
+// and merge hem wit $pages
+foreach ($pages as $k=>$p)
+    $ppages[$k] = $p;
+foreach ($npages as $k=>$p)
+    $ppages[$k] = $p;
+$pages = $ppages;
+
+
+// display!
+
+foreach ($pages as $label => $page)
+{
+    $current = $page == $t->_current_page;
+    $out_of_boundry = $page<1 || $page>$t->_total_pages;
+
+    $classes = array('page', $f->isInt($label)?'num':'text');
+    if ($current)
+        $classes[] = 'active';
+    if ($out_of_boundry)
+        $classes[] = 'disabled';
+    if ($page == 1)
+        $classes[] = 'first';
+    else if ($page == $t->_total_pages)
+        $classes[] = 'last';
+	if ($label === $label_prev)
+		$classes[] = 'prev';
+	else if ($label === $label_next)
+		$classes[] = 'next';
+
+    printf('<span class="%s">', join(' ',$classes));
+    if ($current)
+        printf('<strong>%s</strong>', $t->trans($label));
+    elseif ($out_of_boundry || null === $page)
+        printf('<span>%s</span>', $t->trans($label));
+    else
+        printf('%s', $t->l2cInside($t->trans($label), null, '', array($page)), $label);
+    echo '</span>';
+}
+?>
+</div> <!-- .paginator -->
+
+<?php
+return true;
+
