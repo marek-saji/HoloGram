5474bde043bc19440327f6888f85de4a981f1e36
Marek Augustynowicz <m.augustynowicz@holonglobe.com>
2010-09-07 16:51:38 +0200

    Revert "not using def val when some val was set by user"
    This reverts commit 15fe72a6ea29944aae940c40355455e32139bc70.
    
    empty(false) returns true!

--- lib/Fields.php
+++ lib/Fields.php
@@ -534,7 +534,7 @@ abstract class Field implements IModelField
             {
                 // DEFAULT
                 case 'DEFAULT' === $def['source'] :
-                    if (null===$action || 'update' === $action || !empty($value))
+                    if (null===$action || 'update' === $action)
                     {
                         return false;
                     }
