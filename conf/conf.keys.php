3465504b1646eacbf2991f8548c1b6bd593ed451
Marek Augustynowicz <m.augustynowicz@holonglobe.com>
2010-09-14 16:17:48 +0200

    recaptcha keys moved to app/

--- conf/conf.keys.php
+++ conf/conf.keys.php
@@ -5,8 +5,8 @@ $conf['keys'] = array(
     // reCAPTCHA (domain-specific)
     // https://www.google.com/recaptcha/admin/create
     'recaptcha' => array(
-        'public'  => '6LfGDr0SAAAAABaRpNsbE8sYa4YOs6mEnijj5pp8',
-        'private' => '6LfGDr0SAAAAAPzwesgBXewT2xU1HgqMXKL1LCtR',
+        'public'  => null,
+        'private' => null,
     ),
 
     // Google Maps (domain-specific)
