c4d7413cf9b7a6be2ca28d112db13f19aa1f244f
Marek Augustynowicz <m.augustynowicz@holonglobe.com>
2010-10-25 17:56:29 +0200

    removing references to xinha in FRich as it does not exist in hologram

---
+++ tpl/wysiwyg/init.php
@@ -0,0 +1,8 @@
+<?php
+/**
+ * Dummy template, should be overwritten in app
+ * @author m.augustynowicz
+ */
+
+trigger_error('WYSIWYG initialization should be overwritten in app. And it\'s not.', E_USER_WARNING);
+
