2591c337a06cc9e17297a4c1930a031179398536
Bartlomiej Matuszewski <b.matuszewski@holonglobe.com>
2010-11-03 10:04:02 +0100

    request->_port treated differently
    i've set req->_port to false as default. it was used in req->getBaseUri() and was set to 80 by default. it caused problem when ssl was used (<img src="https://inwestornia.pl:80/upload/obrazek42.png" alt="" />). now server should handle this. was also thinking of making $conf['port'] but sl decided it's bad idea.

--- lib/class.Request.php
+++ lib/class.Request.php
@@ -121,7 +121,7 @@ class Request extends HgBase
      * Port podany w adresie URL
      * @var integer
      */
-    protected $_port = 80;
+    protected $_port = false;
 
     /**
      * Base Uri. First part of request uri not parsed to {@uses $_tree}.
@@ -399,7 +399,7 @@ class Request extends HgBase
                 21  => 'ftp',
                 70  => 'gopher',
             );
-            if (@$services[$this->_port] != $this->_protocol)
+            if ($this->_port && @$services[$this->_port] != $this->_protocol)
             {
                 $host .= ':' . $this->_port;
             }
