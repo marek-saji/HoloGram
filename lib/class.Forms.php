dcb5e25e5f19e8a201600b9617afcf73e071b367
Michal Jutkiewicz <m.jutkiewicz@holonglobe.com>
2010-11-25 17:34:55 +0100

    var_dump deleted

--- lib/class.Forms.php
+++ lib/class.Forms.php
@@ -128,7 +128,6 @@ class Forms extends HgBase
     public function create($action='', array $additional_params=array())
     {
         $f = g('Functions');
-        var_dump($additional_params);
 
         if(!empty($additional_params['ident']))
             $this->__ident = $additional_params['ident'];
