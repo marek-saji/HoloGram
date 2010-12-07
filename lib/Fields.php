b2c4f1f62eb069c9a00ce88078c0f3e8cbd0c29a
Bartlomiej Matuszewski <b.matuszewski@holonglobe.com>
2010-12-07 11:34:36 +0100

    \n\r => \n replacement (FString)

--- lib/Fields.php
+++ lib/Fields.php
@@ -706,6 +706,7 @@ class FString extends Field
         $err = array();
         if(NULL !== $value && $value !== '')
         {
+            $value = str_replace("\r\n", "\n", $value);
             $length = mb_strlen($value, 'utf-8');
             //null is not the same as an empty string
             //if(isset($this->_rules['notnull']) && $this->_rules['notnull'] && !$length)
