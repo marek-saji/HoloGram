76816c130f10dcd17d5d0b174445a1022136ba8e
Marek Augustynowicz <m.augustynowicz@holonglobe.com>
2012-04-15 20:02:26 +0200

    update elseworlds urls in app-acheme debug config

--- app-scheme/app/conf/conf.debug.php
+++ app-scheme/app/conf/conf.debug.php
@@ -6,18 +6,19 @@
 
 $conf['debug']['shortcuts'] = array(
     // eighter literal link
+    //'bugtracker'    => 'bugs.example.com',
     //'go to content' => '#content',
     // .. or array with url2c() arguments
     //'do stuff' => array('Dev', 'doStuff')
 );
 
 $conf['debug']['alternative base URLs'] = array(
-    //'local'         => 'http://example.local/',
-    //'dev'           => 'http://example.holonglobe.pri/',
-    //'test debug'    => 'http://debug.1.1.example.test.1cm.pl/',
-    //'test'          =>       'http://1.1.example.test.1cm.pl/',
-    //'pprod debug'   => 'http://debug.1.1.example.hgsrv05.holonglobe.com/',
-    //'pprod'         =>       'http://1.1.example.hgsrv05.holonglobe.com/',
-    //'prod'          => 'http://example.com/',
+    //'local'         =>             'http://example.localhost/',
+    //'dev'           =>             'http://example.localdomain/',
+    //'test debug'    =>  'http://debug.test.example.com/',
+    //'test'          =>        'http://test.example.com/',
+    //'pprod debug'   => 'http://debug.pprod.example.com/',
+    //'pprod'         =>       'http://pprod.example.com/',
+    //'prod'          =>             'http://example.com/',
 );
 
